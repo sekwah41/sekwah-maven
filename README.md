42e209832edcb83a8154e67d38f23f7a7505151a
Sekwah <sekwah41@gmail.com>
2021-05-05 01:49:04 +0100

    Setting up the basics

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-sekwahmaven
+Sekwah Maven
+=============
+
+run `npm run deploy -- --stage production` to deploy to production
---
+++ maven.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports.folderredirect = (event, context, callback) => {
+    // Extract the request from the CloudFront event that is sent to Lambda@Edge
+    const request = event.Records[0].cf.request;
+
+    // Extract the URI and params from the request
+    const olduri = request.uri;
+
+    // Match any uri that ends with some combination of
+    // [0-9][a-z][A-Z]_- and append a slash
+    var endslashuri = olduri.replace(/(\/[\w\-]+)$/, '$1something/');
+
+    if(endslashuri != olduri) {
+
+        let params = '';
+        if(('querystring' in request) && (request.querystring.length>0)) {
+            params = '?'+request.querystring;
+        }
+        const newUri = endslashuri + params;
+
+        const response = {
+            status: '301',
+            statusDescription: 'Permanently moved',
+            headers: {
+                location: [{
+                    key: 'Location',
+                    value: newUri
+                }]
+            }
+        };
+        return callback(null, response);
+    } else {
+        // Return to CloudFront
+        const request = event.Records[0].cf.request;
+
+        // Extract the URI from the request
+        const oldUri = request.uri;
+
+        const newuri = oldUri.replace(/\/$/, '\/index.html');
+
+        request.uri = newuri;
+
+        return callback(null, request);
+    }
+};
