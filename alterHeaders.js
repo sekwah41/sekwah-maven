efbb5bc600cbf08eda5aceb120f86af5019d2022
Sekwah <contact@sekwah.com>
2021-07-07 22:21:54 +0100

    feat: Adding timeouts and making functions smaller

---
+++ alterHeaders.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports.fileheaders = (event, context, callback) => {
+    const response = event.Records[0].cf.response;
+    const headers = response.headers;
+
+    headers['x-serverless-time'] = [{ key: 'x-serverless-time', value: Date.now().toString() }];
+
+    return callback(null, response);
+};
