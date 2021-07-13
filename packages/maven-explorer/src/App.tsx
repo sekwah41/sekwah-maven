1f35a35b41669c285968645e906ab3c90ebacf87
Sekwah <contact@sekwah.com>
2021-07-13 02:50:40 +0100

    feat: Set basic layout

--- packages/maven-explorer/src/App.tsx
+++ packages/maven-explorer/src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import {Breadcrumb, Layout} from "antd";
+import {Content} from 'antd/lib/layout/layout';
+import {Footer} from "antd/es/layout/layout";
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Layout className="layout">
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>com</Breadcrumb.Item>
+            <Breadcrumb.Item>sekwah</Breadcrumb.Item>
+            <Breadcrumb.Item>SekCLib</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-content">Content</div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Sekwah Maven - Want to ask about a library? <a href={"http://discord.sekwah.com/"}>Join the Discord!</a></Footer>
+      </Layout>
     </div>
   );
 }
