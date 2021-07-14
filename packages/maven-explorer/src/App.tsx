7b3af132f50b51ea2840ae6d186bdc8c1e39b6a5
Sekwah <contact@sekwah.com>
2021-07-14 02:40:03 +0100

    feat: Path now follows structure in url

--- packages/maven-explorer/src/App.tsx
+++ packages/maven-explorer/src/App.tsx
@@ -3,24 +3,45 @@ import './App.scss';
 import {Breadcrumb, Layout} from "antd";
 import {Content} from 'antd/lib/layout/layout';
 import {Footer} from "antd/es/layout/layout";
+import {
+  Link,
+  useLocation,
+} from "react-router-dom";
 
 function App() {
+  let location = useLocation();
+  let path = location.pathname.split("/").filter(a => a !== "");
+
+  const folders: ({ link: string; text: string })[] = [
+    {
+      link: "/",
+      text: "Home"
+    },
+    ...path.map((folder, i) => {
+      return {
+        link: `/${path.slice(0, i + 1).join("/")}/`,
+        text: folder
+      }
+    })
+  ]
+
+  ;
 
   return (
     <div className="App">
-
-      <Layout className="layout">
-        <Content style={{ padding: '0 50px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>com</Breadcrumb.Item>
-            <Breadcrumb.Item>sekwah</Breadcrumb.Item>
-            <Breadcrumb.Item>SekCLib</Breadcrumb.Item>
-          </Breadcrumb>
-          <div className="site-layout-content">Content</div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Sekwah Maven - Want to ask about a library? <a href={"http://discord.sekwah.com/"}>Join the Discord!</a></Footer>
-      </Layout>
+        <Layout className="layout">
+          <Content style={{ padding: '0 50px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              {folders.map(ele => {
+                return (<Breadcrumb.Item>
+                  <Link to={ele.link} key={ele.link}>{ele.text}</Link>
+                </Breadcrumb.Item>)
+              })}
+            </Breadcrumb>
+            <div className="site-layout-content">Content</div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Sekwah Maven - Want to ask about a library? <a href={"http://discord.sekwah.com/"}>Join the Discord!</a></Footer>
+        </Layout>
     </div>
   );
 }
